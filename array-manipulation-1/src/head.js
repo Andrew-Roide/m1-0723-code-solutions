c21715bd6fd6c2aa2233cb09cde4edaf60b70060
Andrew Roide <andrew.roide@hotmail.com>
2023-07-26 04:29:54 +0000

    completed array manipulation 1

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,12 @@
+/* exported head */
+
+/*
+  define a function called head with one parameter called array
+  get the first index of the array parameter and set it to a variable called firstIndex
+  return the value of firstIndex
+*/
+
+function head(array) {
+  const firstIndex = array[0];
+  return firstIndex;
+}
---
+++ array-manipulation-1/src/initial.js
@@ -0,0 +1,9 @@
+/* exported initial */
+
+function initial(array) {
+  const myArray = [];
+  for (let i = 0; i < array.length - 1; i++) {
+    myArray.push(array[i]);
+  }
+  return myArray;
+}
