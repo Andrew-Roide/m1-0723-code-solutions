4fc037f1c4156cd0d15b66e67167b5b52ec3c459
Andrew Roide <andrew.roide@hotmail.com>
2023-08-01 02:47:02 +0000

    Completed array manipulation 2

---
+++ array-manipulation-2/src/take.js
@@ -0,0 +1,9 @@
+/* exported take */
+/*
+  define a function called take with two parameters, array and count
+    take the array, return a portion of the array into a new array from the start of the array index and the count index .
+*/
+
+function take(array, count) {
+  return array.slice(0, count);
+}
