218bf9c2c6ad5c7d9c2859b183f589d01c06497e
Andrew Roide <andrew.roide@hotmail.com>
2023-08-23 02:30:22 +0000

    Completed dom querying

---
+++ dom-querying/script.js
@@ -0,0 +1,17 @@
+const exH1 = document.querySelector('h1');
+console.log(exH1);
+console.dir(exH1);
+
+const explanation = document.querySelector('#explanation');
+console.log(explanation);
+console.dir(explanation);
+
+const hint = document.querySelector('.hint');
+console.log(hint);
+console.dir(hint);
+
+const paragraph = document.querySelectorAll('p');
+console.dir(paragraph);
+
+const exampleLink = document.querySelectorAll('.example-link');
+console.dir(exampleLink);
